3704140801b61a1e32832040729c801e3f1b5b5f
Teemu Ikonen <tpikonen@gmail.com>
2008-09-12 19:03:44 +0200

    Rewrite with one sed invocation, also more correct

--- parsealiases.sh
+++ parsealiases.sh
@@ -5,9 +5,21 @@ UNAME=$2
 
 echo "user is: $UNAME"
 echo "file is: $FNAME"
-# 1st sed: grab the correct lines (FIXME: continuing lines starting from the 
-# first column are included, this should not be)
-# 2nd sed: split tokens to lines
-# 3rd sed: trim leading whitespace
-cat $FNAME | sed -n "1h;1!H;\${;g;s/.*$UNAME:\s*\([^:]*\)\n\S.*/\1/p;}" \
-| sed 's/\(\S\)\s\s*\(\S*\)/\1\n\2/' | sed 's/^\s*//'
+cat $FNAME | sed -n "
+/^$UNAME:.*/ {
+    s/^$UNAME:\s*\(.*\)/\1/ 
+    h
+    :loop
+    n
+    s/^#.*//
+    t loop
+    s/^\S.*//
+    t exit
+    H
+    b loop 
+    :exit
+    x
+    s/\n//g
+    s/\s*,\s*/\n/g
+    p
+}"

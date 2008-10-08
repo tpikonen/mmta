e36cf55833863764b4c418bff010df7b93a39ac9
Teemu Ikonen <tpikonen@gmail.com>
2008-10-08 17:32:06 +0200

    mmda.c: Remove functions runscript(), catforward(), maildirmail()

--- mmda.c
+++ mmda.c
@@ -146,78 +146,6 @@ int mboxmail(FILE* infile, const char *mboxname, const char *cname)
 }
 
 
-void maildirmail(const char *uname, int uid, const char *homedir)
-{
-    char *safecat = SAFECAT;
-    char mdirtmp[SLEN];
-    char mdirnew[SLEN];
-    char *argv[4];
-    struct stat ss;
-
-    /* Check maildir components and perms */
-    snprintf(mdirtmp, SLEN-1, "%s/%s%s/%s", MAILDIR, uname, QEXT, "tmp");
-    if(stat(mdirtmp, &ss) < 0 || ss.st_uid != uid /* || ss.st_gid != MAIL_GID */
-            || !S_ISDIR(ss.st_mode) /* || (ss.st_mode & 0777) != 0660 */ )
-    {
-        exit(1);
-    }
-    snprintf(mdirnew, SLEN-1, "%s/%s%s/%s", MAILDIR, uname, QEXT, "new");
-    if(stat(mdirnew, &ss) < 0 || ss.st_uid != uid /* || ss.st_gid != MAIL_GID */
-            || !S_ISDIR(ss.st_mode) /* || (ss.st_mode & 0777) != 0660 */ )
-    {
-        exit(1);
-    }
-    argv[0] = safecat;
-    argv[1] = mdirtmp;
-    argv[2] = mdirnew;
-    argv[3] = NULL;
-    execprog(argv, homedir);
-}
-
-
-/* Output users' ~/.forward file to stdout
- * Return values:
- * 0 if file exists, is owned by user or root and can be read 
- * 1 if the file does not exist, or is not owned by user or root
- * 2 if there's some other error
- */
-void catforward(int uid, const char *homedir)
-{
-    char fwdname[SLEN];
-    int fd, nread, nwritten;
-    char *buf[SLEN];
-    struct stat ss;
-    int statret;
-
-    strncpy(fwdname, homedir, SLEN-10);
-    strncat(fwdname, "/.forward", 10);
-    statret = stat(fwdname, &ss);
-    if((statret != 0) || ((ss.st_uid != uid) && (ss.st_uid != 0))) {
-        /* .forward does not exist or is not owned by user or root */
-        exit(1);
-    }
-
-    fd = open(fwdname, O_RDONLY);
-    if(fd < 3) {
-        exit(2);
-    }
-    while((nread = read(fd, buf, SLEN)) > 0) {
-        nwritten = 0;
-        while(nwritten < nread) {
-            int nout;
-            nout = write(1, buf, nread-nwritten);
-            if(nout < 0) /* write error */
-                exit(2);
-            nwritten += nout;
-        }
-    }
-    if(nread < 0) { /* read error */
-        exit(2);
-    }
-    exit(0);
-}
-
-
 void eat_wspace(char *buf)
 {
     int i;
@@ -329,17 +257,6 @@ void runforward(const char *fwdname, FILE *mfile, const char *uname,
         // FIXME: Maybe run free() on sargv[>0]
     }
     exit(0);
-}
-
-void runscript(const char *scriptname, const char *uname, const char *homedir)
-{
-    char *argv[2];
-    char fullname[SLEN];
-
-    snprintf(fullname, SLEN-1, "%s/%s", SCRIPTDIR, scriptname);
-    argv[0] = fullname;
-    argv[1] = NULL;
-    execprog(argv, homedir);
 }
 
 
